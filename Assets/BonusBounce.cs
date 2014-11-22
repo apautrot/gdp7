88ff4ec56a0f797bd549ec6de4485cf0663faeac
Alexis Pautrot <alexis@pautrot.com>
2014-11-22 23:49:03 +0100

    Player prefab & sounds

---
+++ Assets/BonusBounce.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BonusBounce : MonoBehaviour
+{
+	public float bounceDuration = 1;
+	public Vector2 bounceOffset = new Vector2 ( 0, 0 );
+
+	void Restart ()
+	{
+		Start ();
+	}
+
+	void Start ()
+	{
+		gameObject.transform.animateBounce ( bounceDuration, bounceOffset.x, bounceOffset.y ).loopsInfinitely ();
+	}	
+}
---
+++ Assets/Sounds/Sounds.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Sounds : SceneSingleton<Sounds>
+{
+	public AudioClip playerMove;
+	public AudioClip playerMoveStart;
+	public AudioClip playerAttack;
+	public AudioClip playerAttackHit;
+	public AudioClip playerExplosion;
+	public AudioClip enemyMove;
+	public AudioClip enemyAttackHit;
+	public AudioClip enemyExplosion;
+	public AudioClip motherKeenMove;
+	public AudioClip motherActivation;
+	public AudioClip motherDesactivation;
+	public AudioClip bonusGrabbed;
+}
