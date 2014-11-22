620fbc4fa14d990cbd042c2896b4af5e2097d130
Alexis Pautrot <alexis@pautrot.com>
2014-11-22 16:15:51 +0100

    Look at camera position script

---
+++ Assets/LookAtCameraPosition.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class LookAtCameraPosition : MonoBehaviour
+{
+	public bool lookAtBaseOfCamera;
+
+#if UNITY_EDITOR
+	void Update ()
+	{
+		FixedUpdate ();
+	}
+#endif
+
+	void FixedUpdate ()
+	{
+		Vector3 targetPosition = transform.position - Camera.main.transform.position;
+		if ( lookAtBaseOfCamera )
+		{
+			targetPosition.y = 0;
+		}
+
+		transform.rotation = Quaternion.LookRotation ( targetPosition, Vector3.up );
+	}
+}
