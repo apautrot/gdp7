ca9303da5a55a2c0b59e43c3eddc881b0928682e
cazzoo <bellecour@D24-02.bellecour.local>
2014-11-23 17:04:22 +0100

    Add endGameScreen + setup player & enemy prefabs

---
+++ Assets/endGame.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class endGame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void OnTriggerEnter(Collider collider) {
+		if(collider.gameObject.tag == "Player") {
+			Application.LoadLevel("End Game");
+		}
+	}
+}
